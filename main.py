6b7054f4bd45b26c8a74a2ec3cba7bf9f221880b
ASYildiz88 <ay1@kent.ac.uk>
2026-03-26 20:54:13 +0000

    Add counting of INFO WARNING and ERROR logs

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import re
 
 file = open("logs.txt", "r")
-
 lines = file.readlines()
+
+info_count = 0
+warning_count = 0
+error_count = 0
 
 for line in lines:
     if re.search("ERROR", line):
-        print("ERROR:", line)
-
+        error_count += 1
     elif re.search("WARNING", line):
-        print("WARNING:", line)
-
+        warning_count += 1
     elif re.search("INFO", line):
-        print("INFO:", line)
+        info_count += 1
+
+print("INFO count:", info_count)
+print("WARNING count:", warning_count)
+print("ERROR count:", error_count)
 
 file.close()
