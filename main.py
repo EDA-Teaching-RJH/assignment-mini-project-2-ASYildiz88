ba82ed82f9c73f43d06a254759080fdac8b2c060
ASYildiz88 <ay1@kent.ac.uk>
2026-03-26 20:59:24 +0000

    Refactor log counting into a function

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import re
 
-file = open("logs.txt", "r")
-lines = file.readlines()
 
-info_count = 0
-warning_count = 0
-error_count = 0
+def count_logs(lines):
+    info_count = 0
+    warning_count = 0
+    error_count = 0
 
-for line in lines:
-    if re.search("ERROR", line):
-        error_count += 1
-    elif re.search("WARNING", line):
-        warning_count += 1
-    elif re.search("INFO", line):
-        info_count += 1
+    for line in lines:
+        if re.search("ERROR", line):
+            error_count += 1
+        elif re.search("WARNING", line):
+            warning_count += 1
+        elif re.search("INFO", line):
+            info_count += 1
 
-print("INFO count:", info_count)
-print("WARNING count:", warning_count)
-print("ERROR count:", error_count)
+    return info_count, warning_count, error_count
 
-file.close()
+
+def main():
+    file = open("logs.txt", "r")
+    lines = file.readlines()
+
+    info_count, warning_count, error_count = count_logs(lines)
+
+    print("INFO count:", info_count)
+    print("WARNING count:", warning_count)
+    print("ERROR count:", error_count)
+
+    file.close()
+
+
+main()
